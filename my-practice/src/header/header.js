b57a7f31b25f826bedc04e7b2e4e59d62f24b73e
Vidhya K <vidhyakandhasamy1@gmail.com>
2025-11-19 23:26:52 +0530

    changes bhhbhj

--- my-practice/src/header/header.js
+++ my-practice/src/header/header.js
@@ -10,12 +10,22 @@ export default function Header() {
             <img src={headerlogo} alt="Logo" className="logo-image"/>
         </div>
         <nav className="nav">
-            <a href="#home">Home</a>
-            <a href="#features">Features</a>
-            <a href="#community">Community</a>
-            <a href="#blog">Blog</a>
-            <a href="#contact">Pricing</a>
-            <a href="#register" className="login-button">Register Now<span><img src={arrowicon}/></span></a>
+          <ul>
+            <li> <a href="#home">Home</a></li>
+            <li>Features
+            <div>
+                <ul className="dropdown">
+                    <li><a href="#feature1">Feature 1</a></li>
+                    <li><a href="#feature2">Feature 2</a></li>
+                    <li><a href="#feature3">Feature 3</a></li>
+                </ul>
+              </div>
+              </li>
+            <li> <a href="#community">Community</a></li>
+            <li> <a href="#blog">Blog</a></li>
+            <li> <a href="#contact">Pricing</a></li>
+            <li> <a href="#register" className="login-button">Register Now<span><img src={arrowicon}/></span></a></li>
+          </ul>
         </nav>  
     </div>
     </header>
